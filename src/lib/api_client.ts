0881500a01b3e25900384539a9ca1ce69028c647
Luke Fox <lukefox5@gmail.com>
2026-05-24 12:04:13 +0100

    fixed base URL

--- src/lib/api_client.ts
+++ src/lib/api_client.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import supabase from "./supabase_client"
 
 const apiClient = axios.create({
-  baseURL: new URL(import.meta.env.VITE_API_URL).pathname,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
